31b3d0e32b18b8c7fa9f2436cee0baf29ad3c6c8
nnieddu <nicolas.nieddu@free.fr>
2021-11-23 20:07:28 +0100

    begin

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,12 +6,25 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 19:49:35 by ninieddu          #+#    #+#             */
-/*   Updated: 2021/11/23 19:50:30 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2021/11/23 20:06:04 by ninieddu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     main()
+#include <iostream>
+
+std::string	ft_to_char(const char *s)
 {
     
-    return (0);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 2)
+    {
+        std::cout << ft_to_char(argv[1]) << std::endl;
+        std::cout << ft_to_int(argv[1]) << std::endl;
+        std::cout << ft_to_float(argv[1]) << std::endl;
+        std::cout << ft_to_double(argv[1]) << std::endl;
+    }
+	return (0);
 }
