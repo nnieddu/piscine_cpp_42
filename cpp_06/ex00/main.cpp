3f857e36295bd2c4febef25b20d1b3625d91fbd7
nnieddu <nicolas.nieddu@free.fr>
2021-11-24 17:49:15 +0100

    go

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,25 +6,81 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 19:49:35 by ninieddu          #+#    #+#             */
-/*   Updated: 2021/11/23 20:06:04 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2021/11/24 17:48:15 by ninieddu         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cmath> 
+#include <ctgmath>
+#include <cstdlib>
 
-std::string	ft_to_char(const char *s)
+void	toChar(double arg)
 {
-    
+	std::cout << "char: ";
+	if (arg < CHAR_MIN || arg > CHAR_MAX || isnan(arg))
+	{
+		std::cout << "Impossible" << std::endl;
+		return ;
+	}
+	if (!isprint(static_cast<char>(arg)))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << "'" << static_cast<char>(arg) << "'" << std::endl;
 }
 
-int		main(int argc, char *argv[])
+void	toInt(double arg)
 {
-	if (argc == 2)
-    {
-        std::cout << ft_to_char(argv[1]) << std::endl;
-        std::cout << ft_to_int(argv[1]) << std::endl;
-        std::cout << ft_to_float(argv[1]) << std::endl;
-        std::cout << ft_to_double(argv[1]) << std::endl;
-    }
+	std::cout << "int: ";
+	if (static_cast<int>(arg) > INT_MAX || static_cast<int>(arg) < INT_MIN || isnan(arg))
+	{
+		std::cout << "Impossible" << std::endl; 
+		return ;
+	}	
+	std::cout << static_cast<int>(arg) << std::endl;	
+}
+
+void	toFloat(double arg) 
+{
+	std::cout << "float: ";
+	if (static_cast<float>(arg))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << static_cast<float>(arg) << "f" << std::endl;
+}
+
+void	toDouble(double arg) 
+{
+		std::cout << "double: ";
+	if (static_cast<double>(arg) == HUGE_VAL)
+	{
+		std::cout << "impossible" << std::endl;
+		return ;
+	}
+	std::cout << arg << std::endl;
+}
+
+int		main(int ac, char *av[])
+{
+	double arg;
+
+	if (ac == 2)
+	{
+		arg = strtod(av[1], NULL);
+		if (arg == 0)
+		{
+			if (strlen(av[1]) > 1) 
+				arg = av[1][0];
+		}			
+			toChar(arg);
+			toInt(arg);
+			toFloat(arg);
+			toDouble(arg);
+		}
+		else
+		{
+			std::cerr << "Nothing, to convert, need arg !" << std::endl;
+			return(-1);
+		}
 	return (0);
 }
