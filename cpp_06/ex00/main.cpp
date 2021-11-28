a1e04f07294871c337ff777eed10f6b451992200
nnieddu <ninieddu@student.42lyon.fr>
2021-11-28 14:45:06 -0500

    finish

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -6,21 +6,22 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 19:49:35 by ninieddu          #+#    #+#             */
-/*   Updated: 2021/11/24 17:48:15 by ninieddu         ###   ########lyon.fr   */
+/*   Updated: 2021/11/28 13:20:25 by ninieddu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cmath> 
-#include <ctgmath>
 #include <cstdlib>
+#include <climits>
+#include <cstring>
 
 void	toChar(double arg)
 {
 	std::cout << "char: ";
-	if (arg < CHAR_MIN || arg > CHAR_MAX || isnan(arg))
+	if (arg < CHAR_MIN || arg > CHAR_MAX || std::isnan(arg))
 	{
-		std::cout << "Impossible" << std::endl;
+		std::cout << "impossible" << std::endl;
 		return ;
 	}
 	if (!isprint(static_cast<char>(arg)))
@@ -32,18 +33,18 @@ void	toChar(double arg)
 void	toInt(double arg)
 {
 	std::cout << "int: ";
-	if (static_cast<int>(arg) > INT_MAX || static_cast<int>(arg) < INT_MIN || isnan(arg))
+	if (static_cast<int>(arg) > INT_MAX || static_cast<int>(arg) < INT_MIN || std::isnan(arg))
 	{
-		std::cout << "Impossible" << std::endl; 
+		std::cout << "impossible" << std::endl; 
 		return ;
-	}	
+	}
 	std::cout << static_cast<int>(arg) << std::endl;	
 }
 
 void	toFloat(double arg) 
 {
 	std::cout << "float: ";
-	if (static_cast<float>(arg))
+	if (static_cast<float>(arg) == HUGE_VAL)
 		std::cout << "impossible" << std::endl;
 	else
 		std::cout << static_cast<float>(arg) << "f" << std::endl;
@@ -51,7 +52,7 @@ void	toFloat(double arg)
 
 void	toDouble(double arg) 
 {
-		std::cout << "double: ";
+	std::cout << "double: ";
 	if (static_cast<double>(arg) == HUGE_VAL)
 	{
 		std::cout << "impossible" << std::endl;
@@ -60,18 +61,20 @@ void	toDouble(double arg)
 	std::cout << arg << std::endl;
 }
 
-int		main(int ac, char *av[])
+int	main(int ac, char **av)
 {
-	double arg;
-
+	double arg = 0;
+	
 	if (ac == 2)
 	{
+		std::cout << arg << std::endl;
 		arg = strtod(av[1], NULL);
+		std::cout << arg;
 		if (arg == 0)
 		{
-			if (strlen(av[1]) > 1) 
-				arg = av[1][0];
-		}			
+		 if (strlen(av[1]) > 1)
+		 	arg = av[1][0];
+		}
 			toChar(arg);
 			toInt(arg);
 			toFloat(arg);
