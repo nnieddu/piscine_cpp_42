bd08ae70557925c0f9c297ffe1720ad954a24737
nnieddu <ninieddu@student.42lyon.fr>
2021-12-03 08:15:52 -0500

    clean

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 16:55:50 by ninieddu          #+#    #+#             */
-/*   Updated: 2021/12/03 07:41:19 by ninieddu         ###   ########.fr       */
+/*   Updated: 2021/12/03 08:15:29 by ninieddu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,34 +41,33 @@ int	main()
 	}
 	std::stack<int> s(mstack);
 
-	// std::cout << std::endl << std::endl;
+	std::cout << std::endl << "Liste :" << std::endl;
+	std::list<int> mstack_l;
 
-	// std::list<int> mstack_l;
-	// mstack_l.push_back(5);
-	// mstack_l.push_back(17);
+	mstack_l.push_back(5);
+	mstack_l.push_back(17);
 	
-	// // std::cout << mstack_l.top() << std::endl << std::endl;
+	std::cout << mstack_l.back() << std::endl << std::endl;
 	
-	// // mstack_l.pop();
+	mstack_l.pop_back();
 
-	// // std::cout << mstack_l.size() << std::endl << std::endl;
+	std::cout << mstack_l.size() << std::endl << std::endl;
 	
-	// mstack_l.push_back(3);
-	// mstack_l.push_back(5);
-	// mstack_l.push_back(737);
-	// mstack_l.push_back(0);
+	mstack_l.push_back(3);
+	mstack_l.push_back(5);
+	mstack_l.push_back(737);
+	mstack_l.push_back(0);
 	
-	// std::list<int>::iterator it_l = mstack_l.begin();
-	// std::list<int>::iterator ite_l = mstack_l.end();
+	std::list<int>::iterator it_l = mstack_l.begin();
+	std::list<int>::iterator ite_l = mstack_l.end();
 
-	// ++it_l;
-	// --it_l;
-	// while (it_l != ite_l)
-	// {
-	// 	std::cout << *it_l << std::endl;
-	// 	++it_l;
-	// }
-	// std::stack<int> s(mstack_l);
+	++it_l;
+	--it_l;
+	while (it_l != ite_l)
+	{
+		std::cout << *it_l << std::endl;
+		++it_l;
+	}
 	
 	return 0;
 }
--- cpp_08/ex02/mutantstack.hpp
+++ cpp_08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: ninieddu <ninieddu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 05:06:37 by ninieddu          #+#    #+#             */
-/*   Updated: 2021/12/03 07:41:38 by ninieddu         ###   ########.fr       */
+/*   Updated: 2021/12/03 08:07:20 by ninieddu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,25 @@
 template<typename T>
 class MutantStack : public std::stack<T> 
 {
-private:
+    public:
+        MutantStack() {}
+        MutantStack(const MutantStack &src){}
+        virtual ~MutantStack() {}
+        MutantStack &operator=(const MutantStack &src) 
+        {
+            std::stack<T>::operator=(src);
+        }
     
-public:
-    MutantStack() : std::stack<T>() {}
-    virtual ~MutantStack() {}
-    MutantStack(const MutantStack &src) : std::stack<T>(src) {}
-    MutantStack &operator=(const MutantStack &src) 
-    {
-        std::stack<T>::operator=(src);
-    }
-
-    typedef typename std::stack<T>::container_type::iterator iterator;
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-
-    iterator begin()
-    {
-        return std::stack<T>::c.begin();
-    }
-
-    iterator end()
-    {
-        return std::stack<T>::c.end();
-    }
-
-    const_iterator cbegin()
-    {
-        return std::stack<T>::c.cbegin();
-    }
-
-    const_iterator cend()
-    {
-        return std::stack<T>::c.cend();
-    }
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin()
+        {
+            return std::stack<T>::c.begin();
+        }
+    
+        iterator end()
+        {
+            return std::stack<T>::c.end();
+        }
 };
-
 
 #endif
